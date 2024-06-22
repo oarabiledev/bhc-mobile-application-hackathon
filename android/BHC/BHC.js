eb8705bff5dded5747ed4018046d8c8a032094bc
Oarabile Koore <153186345+oarabiledev@users.noreply.github.com>
2024-06-22 15:52:27 +0200

    Update BHC.js

--- android/BHC/BHC.js
+++ android/BHC/BHC.js
@@ -9,6 +9,7 @@
  * 
  * I built the material-design plugin 
 */
+
 cfg.MUI
 cfg.Light
 app.LoadPlugin('material-design')
@@ -24,6 +25,22 @@ let isNew = app.LoadBoolean('isNewUser',true,userConfig);
 function OnStart() {
     ui.InitializeMaterialPlugin('light')
     ui.setTheme('light-medium-contrast')
+    
+    /* Here Is The FireBase Config, This Is Not Secure !!! */
+    
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional\
+    const firebaseConfig = {
+        apiKey: "AIzaSyDa8-30xelWHYyGKw82givvwt8eM8KmMqE",
+        authDomain: "bhc-mobile-app.firebaseapp.com",
+        projectId: "bhc-mobile-app",
+        storageBucket: "bhc-mobile-app.appspot.com",
+        messagingSenderId: "309220637622",
+        appId: "1:309220637622:web:b2078df1b709be23e01f17",
+        measurementId: "G-VS9ZLB9C6B"
+    };
+    
+    
+    firebase.initializeApp(firebaseConfig)
     if (isNew) welcomeUser();
     else openApp();
 }
@@ -39,23 +56,44 @@ function welcomeUser(){
     welcomeText.SetFontFile('Misc/lexend.ttf');
     welcomeText.SetTextSize(18)
     
-    loginBar = MUI.AddTEOutlineIconLeft(loginScreen,0.8,'Left','person','type your email',null,'#904A4A')
-    loginBar.SetMargins(null,0.03)
+    emailBar = MUI.AddTEOutlineIconLeft(loginScreen,0.8,'Left','person','type your email',null,'#904A4A')
+    emailBar.SetMargins(null,0.03)
     
     passwordBar = MUI.AddTEOutlineIconLeft(loginScreen,0.8,'Left,Password','lock','account password',null,'#904A4A')
     passwordBar.SetMargins(null,0.03)
     
+    /**
+     * Login Layout For Buttons
+    */
+    
+    buttonsLay = ui.createLayout('Linear','Horizontal,Center');
+    buttonsLay.SetMargins(0, 0.05)
+    buttonsLay.SetSize(0.8,-1)
+    buttonsLay.SetChildMargins(0.05,0,0.05)
+    loginScreen.AddChild(buttonsLay)
+    
+    googleLoginBtn = MUI.AddButtonElegant(buttonsLay, 'logo_google', 0.2, -1,'#904A4A')
+    googleLoginBtn.SetFontFile('Misc/f7Icons.ttf')
+    
+    facebookLoginBtn = MUI.AddButtonElegant(buttonsLay, 'logo_facebook', 0.2, -1,'#904A4A')
+    facebookLoginBtn.SetFontFile('Misc/f7Icons.ttf')
+    
+    appleLoginBtn = MUI.AddButtonElegant(buttonsLay, 'logo_apple', 0.2, -1,'#904A4A')
+    appleLoginBtn.SetFontFile('Misc/f7Icons.ttf')
+    
     forgotPassText = app.AddText(loginScreen,'Forgot Password ?',0.8,null,'Left')
-    forgotPassText.SetMargins(null,0.065)
+    forgotPassText.SetMargins(null,0.05)
     forgotPassText.SetFontFile('Misc/lexend.ttf')
     
     newUserText =  app.AddText(loginScreen,' I Dont Have An Account ',0.8,null,'Left')
     newUserText.SetMargins(null,0.065)
     newUserText.SetFontFile('Misc/lexend.ttf')
     
-    nextButton = ui.addFilledButton('LogIn', 0.8, -1,null, loginScreen);
-    nextButton.SetMargins(0, 0.17, 0, 0)
-    
+    loginButton = ui.addFilledButton('LogIn', 0.8, -1,null, loginScreen);
+    loginButton.SetMargins(0, 0.04, 0, 0)
+    loginButton.SetOnTouch(()=>{
+        signInUserWithEmail(emailBar.GetText(), passwordBar.GetText())
+    })
     app.AddLayout(loginScreen)
 }
 
@@ -64,3 +102,31 @@ function openApp(){
     
 }
 
+
+function signInUserWithEmail(email, password){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        var user = userCredential.user;
+        console.log(JSON.stringify(userCredential.user));
+    })
+   .catch((error) => {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage)
+    });
+}
+
+function signUpuserWithEmail(email, password){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        var user = userCredential.user;
+        console.log(JSON.stringify(userCredential.user));
+    })
+    .catch((error) => {
+       var errorCode = error.code;
+       var errorMessage = error.message;
+       console.log(errorMessage)
+    });
+}
