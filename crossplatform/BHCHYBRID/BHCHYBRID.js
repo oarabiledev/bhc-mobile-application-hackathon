068ce2b825031be2f662ffb2462b272dc700a5aa
Oarabile Koore <153186345+oarabiledev@users.noreply.github.com>
2024-06-26 09:38:15 +0200

    Update BHCHYBRID.js

--- crossplatform/BHCHYBRID/BHCHYBRID.js
+++ crossplatform/BHCHYBRID/BHCHYBRID.js
@@ -27,15 +27,7 @@ app.LoadPlugin("Animator");
 
 app.EnableBackKey(false);
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDa8-30xelWHYyGKw82givvwt8eM8KmMqE",
-    authDomain: "bhc-mobile-app.firebaseapp.com",
-    projectId: "bhc-mobile-app",
-    storageBucket: "bhc-mobile-app.appspot.com",
-    messagingSenderId: "309220637622",
-    appId: "1:309220637622:web:b2078df1b709be23e01f17", 
-    measurementId: "G-VS9ZLB9C6B"
-};
+const firebaseConfig = "REDACTED"
 
 let myriadpro = 'Misc/MYRIADPRO-REGULAR.OTF';
 
